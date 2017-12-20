a3b9d0a110a8fbfc054051da5934e761d1d2b289
kashyap1729 <kashyap.kiran.1729@gmail.com>
2017-12-20 12:07:09 +0530

    Passport

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
+
 
 
 
@@ -55,27 +59,24 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', index);
 app.use('/users', users);
 
 function auth (req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-  if(!req.session.user) {
+    if (!req.user) {
       var err = new Error('You are not authenticated!');
-      err.status = 403;
+      res.setHeader('WWW-Authenticate', 'Basic');                          
+      err.status = 401;
       return next(err);
-  }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
     }
     else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+          next();
     }
-  }
 }
 
 app.use(auth);
