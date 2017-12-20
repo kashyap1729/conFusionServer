ab10b6b0554d03e737d07f9e21c0602764ec2c53
kashyap1729 <kashyap.kiran.1729@gmail.com>
2017-12-20 13:27:11 +0530

    Passport token

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
 
+var config = require('./config');
+
 
 
 
@@ -27,7 +29,8 @@ mongoose.Promise = require('bluebird');
 const Dishes = require('./models/dishes');
 
 // Connection URL
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
+
 const connect = mongoose.connect(url, {
     useMongoClient: true,
     /* other options */
@@ -51,36 +54,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+
 
 app.use(passport.initialize());
-app.use(passport.session());
+
 
 app.use('/', index);
 app.use('/users', users);
-
-function auth (req, res, next) {
-    console.log(req.user);
-
-    if (!req.user) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');                          
-      err.status = 401;
-      return next(err);
-    }
-    else {
-          next();
-    }
-}
-
-app.use(auth);
-
 
 
 
